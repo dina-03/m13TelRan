718c6ac2c66480c26cb73e50b7426bb0b3501834
dina-03 <dingrafikweb@gmail.com>
2020-09-14 13:44:30 +0200

    commit Person full

--- hw04PersonsProject/src/de/telran/application/PersonApp.java
+++ hw04PersonsProject/src/de/telran/application/PersonApp.java
@@ -4,22 +4,38 @@ import de.telran.data.Person;
 
 public class PersonApp {
     public static void main(String[] args) {
-        Person person1 = new Person("Vasya", 30);
+        Person person1 = new Person("Vasya", 30, 1.70);
         person1.setName("Vasiliy");
-        Person person2 = new Person("Alex", 44);
-        Person person3 = new Person("Katya", 18);
-        Person person4 = new Person("Masha", 23);
-        Person person5 = new Person("Anastasiya", 19);
-
+        Person person2 = new Person("Alex", 44, 1.68);
+        Person person3 = new Person("Katya", 18, 1.55);
+        Person person4 = new Person("Masha", 23, 1.60);
+        Person person5 = new Person("Anastasiya", 19, 1.65);
+        Person person6 = new Person("Katya", 18, 1.55);
+        //System.out.println(person3.equals(person6));
         /*Person person1 = new Person("Jakob", 15);
         Person person2 = new Person("Jan", 3);
         Person person3 = new Person("Ekaterina", 10);
         Person person4 = new Person("Olya", 4);
         Person person5 = new Person("Petya", 7);*/
-
-        Person[] group = {person1, person2, person3, person4, person5};
+        Person[] group = {person1, person2, person3, person4, person5, person6};
         Person.displayPersons(group);
-        System.out.println("-------");
+        split();
+        System.out.println("The oldest of the group " + Person.getOldestPerson(group));
+        System.out.println("The longest name in the" + Person.getLongestNamePerson(group));
+       /* split();
+        Person.sortPersonByAge(group);
+        Person.displayPersons(group);
+        split();
+        Person.sortPersonByAge2(group);
+        Person.displayPersons(group);
+        split();
+        Person.sortPersonByName(group);
+        Person.displayPersons(group);
+        split();
+        Person.sortPersonByGrowth(group);
+        Person.displayPersons(group);*/
+
+        /*System.out.println("-------");
         System.out.println("Решение Марии");
         int[] age = {person1.getAge(), person2.getAge(), person3.getAge(), person4.getAge()};
         int maxAge = Person.calculateAge(age);
@@ -32,6 +48,10 @@ public class PersonApp {
         System.out.println("Решение Жени");
         Person.displayOldestPerson(group);
         Person.displayYoungestPerson(group);
-        Person.displayLongestNamePerson(group);
+        Person.displayLongestNamePerson(group);*/
+    }
+
+    public static void split() {
+        System.out.println("-----------");
     }
 }
--- hw04PersonsProject/src/de/telran/data/Person.java
+++ hw04PersonsProject/src/de/telran/data/Person.java
@@ -1,15 +1,16 @@
 package de.telran.data;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Objects;
 
 public class Person {
     private String name;
     private int age;
+    private double growth;
 
-    public Person(String name, int age) {
+    public Person(String name, int age, double growth) {
         this.name = name;
         this.age = age;
+        this.growth = growth;
     }
 
     public String getName() {
@@ -32,9 +33,26 @@ public class Person {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
     public String toString() {
         return "\"Person Name\": " + name +
                 ", age: " + age +
+                ", growth: " + growth +
                 ';';
     }
 
@@ -44,6 +62,101 @@ public class Person {
         }
     }
 
+    public static Person getOldestPerson(Person[] persons) {
+        Person oldestPerson = persons[0];
+        for (int i = 0; i < persons.length; i++) {
+            if (persons[i].age > oldestPerson.age) {
+                oldestPerson = persons[i];
+            }
+        }
+        return oldestPerson;
+    }
+
+    public static Person getLongestNamePerson(Person[] persons) {
+        Person longestName = persons[0];
+        for (int i = 0; i < persons.length; i++) {
+            if (persons[i].name.length() > longestName.name.length()){
+               longestName = persons[i];
+            }
+        }
+        return longestName;
+    }
+
+    private static int comparePersonByAge(Person p1, Person p2) {
+        if (p1.age > p2.age) {
+            return 1;
+        } else if (p1.age < p2.age)
+            return -1;
+        return 0;
+    }
+
+    public static void sortPersonByAge(Person[] persons) {
+        for (int i = 0; i < persons.length - 1; i++) {
+            for (int j = 0; j < (persons.length - 1) - i; j++) {
+                if (comparePersonByAge(persons[j], persons[j + 1]) < 0) {
+                    Person temp = persons[j + 1];
+                    persons[j + 1] = persons[j];
+                    persons[j] = temp;
+                }
+            }
+        }
+    }
+
+    private int compareToPersonByAge(Person person) {
+        if (this.age > person.age)
+            return 1;
+        else if (this.age < person.age)
+            return -1;
+        return 0;
+    }
+
+    private double compareToPersonByGrowth(Person persons) {
+        if (this.growth > persons.growth)
+            return 1;
+        else if (this.growth < persons.growth)
+            return -1;
+        return 0;
+    }
+
+    public static void sortPersonByGrowth(Person[] persons){
+        for (int i = 0; i < persons.length - 1; i++) {
+            for (int j = 0; j < (persons.length - 1) - i; j++) {
+                if (persons[j].compareToPersonByGrowth(persons[j + 1]) > 0){
+                    Person temp = persons[j + 1];
+                    persons[j + 1] = persons[j];
+                    persons[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void sortPersonByAge2(Person[] persons) {
+        for (int i = 0; i < persons.length - 1; i++) {
+            for (int j = 0; j < (persons.length - 1) - i; j++) {
+                if (persons[j].compareToPersonByAge(persons[j + 1]) > 0) {
+                    Person temp = persons[j + 1];
+                    persons[j + 1] = persons[j];
+                    persons[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void sortPersonByName(Person[] persons) {
+        for (int i = 0; i < persons.length - 1; i++) {
+            for (int j = 0; j < (persons.length - 1) - i; j++) {
+                if (persons[j].name.compareTo(persons[j + 1].name) > 0) {
+                    Person temp = persons[j + 1];
+                    persons[j + 1] = persons[j];
+                    persons[j] = temp;
+                }
+            }
+        }
+
+    }
+
+
+/*
     public static Person findOldestPerson(Person[] group, int maxAge) {
         Person p = group[0];
         for (Person person : group) {
@@ -88,9 +201,9 @@ public class Person {
         System.out.println("The oldest of the group " + Person.getOldestPerson(persons));
     }
 
-    /*public static Person getYoungestPerson(Person[] persons) {
+    *//*public static Person getYoungestPerson(Person[] persons) {
         return Arrays.stream(persons).min(Comparator.comparing(Person::getAge)).get();
-    }*/
+    }*//*
 
     public static void displayYoungestPerson(Person[] persons) {
         System.out.println("The youngest of the group " + Arrays.stream(persons).min(Comparator.comparing(person ->
@@ -100,5 +213,6 @@ public class Person {
     public static void displayLongestNamePerson(Person[] persons) {
         System.out.println("The longest name in the " + Arrays.stream(persons).max(Comparator.comparing(person ->
                 person.name.length())).get());
-    }
+    }*/
+
 }
