476a9256a2f155839f968eaac867def5708347a0
dina-03 <dingrafikweb@gmail.com>
2020-10-26 20:14:19 +0100

    Set Demo

---
+++ 20201026_SetMapIssues/src/de/telran/Set.java
@@ -0,0 +1,16 @@
+package de.telran;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+public class Set {
+    public static void main(String[] args) {
+
+    }
+
+    private static List<Integer> removeDuplicates(List<Integer> set){
+        Iterator<Integer> iterator = set.iterator();
+        return null;
+    }
+}
