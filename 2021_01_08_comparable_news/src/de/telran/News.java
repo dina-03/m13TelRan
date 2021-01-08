63417a35a322793fe0cbaca2bd7a31aa9511da1b
dina-03 <dingrafikweb@gmail.com>
2021-01-08 19:22:21 +0100

    News

---
+++ 2021_01_08_comparable_news/src/de/telran/News.java
@@ -0,0 +1,35 @@
+package de.telran;
+
+public class News implements Comparable<News> {
+
+    String text;
+    //1 to 10
+    int positivity;
+
+    public News(String text, int positivity) {
+        this.text = text;
+        this.positivity = positivity;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getPositivity() {
+        return positivity;
+    }
+
+    @Override
+    public int compareTo(News other) {
+        int res = this.positivity - other.positivity;
+        if (res != 0)
+            return res;
+        return other.text.length() - this.text.length();
+    }
+
+    @Override
+    public String toString() {
+        return "News: " + text +
+                ", positivity: " + positivity;
+    }
+}
