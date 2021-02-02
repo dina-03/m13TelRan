7a6a2410662079a1a59f6c7b57023b6d5b5a7f54
dina-03 <dingrafikweb@gmail.com>
2021-02-02 10:07:22 +0100

    L

--- 21_02_01_print_writer/src/de/telran/concatenator/IPrintStream.java
+++ 21_02_01_print_writer/src/de/telran/concatenator/IPrintStream.java
@@ -1,6 +1,7 @@
 package de.telran.concatenator;
 
 import de.telran.IPrint;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -9,13 +10,13 @@ import java.util.List;
 public class IPrintStream implements IPrint {
     @Override
     public void concatenate(List<String> strings, String filename) throws IOException {
-        try (FileOutputStream fos = new FileOutputStream(filename)){
+        try (FileOutputStream fos = new FileOutputStream(filename)) {
 
-        PrintStream ps = new PrintStream(fos);
-        for (String string : strings) {
-            ps.println(string);
-        }
-        ps.close();
+            PrintStream ps = new PrintStream(fos);
+            for (String string : strings) {
+                ps.println(string);
+            }
+            ps.close();
         }
     }
 }
