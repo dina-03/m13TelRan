61cc66c5fb7fc8b7a3466f2569a9316189c56d54
dina-03 <dingrafikweb@gmail.com>
2021-02-02 11:38:23 +0100

    IPrinter Stream/Writer-Print and Printer tester

---
+++ 21_02_02_print_writer_tester/src/de/telran/IPrinter.java
@@ -0,0 +1,8 @@
+package de.telran;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface IPrinter {
+    void writeToFile(String filename, List<String> text) throws IOException;
+}
