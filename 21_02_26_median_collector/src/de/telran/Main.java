8e25ba0f3f2f3446570bbbb2dac94386145b8714
dina-03 <dingrafikweb@gmail.com>
2021-02-28 15:47:49 +0100

    median element collector and test

---
+++ 21_02_26_median_collector/src/de/telran/Main.java
@@ -0,0 +1,12 @@
+package de.telran;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       // List<Integer> numbers =
+
+    }
+}
---
+++ 21_02_26_median_collector/src/de/telran/MedianElementCollectorTest.java
@@ -0,0 +1,24 @@
+package de.telran;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedianElementCollectorTest {
+
+    @Test
+    public void testCollectorTest_oddSizeStream() {
+        Stream<Integer> numbers = Stream.of(5, 8, -10, 15, 7);
+        Double res = numbers.collect(new MedianElementCollector());
+        assertEquals(7, res, 0.00001);
+    }
+
+    @Test
+    public void testCollectorTest_evenSizeStream() {
+        Stream<Integer> numbers = Stream.of(5, 8, -10, 15);
+        Double res = numbers.collect(new MedianElementCollector());
+        assertEquals(6.5, res, 0.00001);
+    }
+}
